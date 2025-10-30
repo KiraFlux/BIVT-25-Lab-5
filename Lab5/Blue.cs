1d10bb41e8a51eb975004c7588dbb1ec82abd79e
KiraFlux <pret2titio1n@gmail.com>
2025-10-30 18:42:27 +0300

    hell code style for Blue.cs

--- Lab5/Blue.cs
+++ Lab5/Blue.cs
@@ -1,12 +1,6 @@
-﻿using System.Linq;
-using System.Runtime.InteropServices;
-
-namespace Lab5
-{
-    public class Blue
-    {
-        public double[] Task1(int[,] matrix)
-        {
+﻿namespace Lab5 {
+    public class Blue {
+        public double[] Task1(int[,] matrix) {
             double[] answer = null;
 
             // code here
@@ -15,8 +9,7 @@ namespace Lab5
 
             return answer;
         }
-        public int[,] Task2(int[,] matrix)
-        {
+        public int[,] Task2(int[,] matrix) {
             int[,] answer = null;
 
             // code here
@@ -25,16 +18,14 @@ namespace Lab5
 
             return answer;
         }
-        public void Task3(int[,] matrix)
-        {
+        public void Task3(int[,] matrix) {
 
             // code here
 
             // end
 
         }
-        public int[,] Task4(int[,] matrix)
-        {
+        public int[,] Task4(int[,] matrix) {
             int[,] answer = null;
 
             // code here
@@ -43,8 +34,7 @@ namespace Lab5
 
             return answer;
         }
-        public int[] Task5(int[,] matrix)
-        {
+        public int[] Task5(int[,] matrix) {
             int[] answer = null;
 
             // code here
@@ -53,56 +43,49 @@ namespace Lab5
 
             return answer;
         }
-        public void Task6(int[,] matrix, int k)
-        {
+        public void Task6(int[,] matrix, int k) {
 
             // code here
 
             // end
 
         }
-        public void Task7(int[,] matrix, int[] array)
-        {
+        public void Task7(int[,] matrix, int[] array) {
 
             // code here
 
             // end
 
         }
-        public void Task8(int[,] matrix)
-        {
+        public void Task8(int[,] matrix) {
 
             // code here
 
             // end
 
         }
-        public void Task9(int[,] matrix)
-        {
+        public void Task9(int[,] matrix) {
 
             // code here
 
             // end
 
         }
-        public void Task10(int[,] matrix)
-        {
+        public void Task10(int[,] matrix) {
 
             // code here
 
             // end
 
         }
-        public void Task11(int[,] matrix)
-        {
+        public void Task11(int[,] matrix) {
 
             // code here
 
             // end
 
         }
-        public int[][] Task12(int[][] array)
-        {
+        public int[][] Task12(int[][] array) {
             int[][] answer = null;
 
             // code here
