0c5c898420850714d5dee9f34c326e660fc0c659
KiraFlux <pret2titio1n@gmail.com>
2025-11-10 12:47:18 +0300

    bruh format

--- Lab5/Green.cs
+++ Lab5/Green.cs
@@ -1,46 +1,33 @@
-﻿using System.Linq;
-using System.Runtime.InteropServices;
-
-namespace Lab5
-{
-    public class Green
-    {
-        public int[] Task1(int[,] matrix)
-        {
+﻿namespace Lab5 {
+    public class Green {
+        public int[] Task1(int[,] matrix) {
             int[] answer = null;
-
             // code here
 
             // end
-
             return answer;
         }
-        public void Task2(int[,] matrix)
-        {
+        
+        public void Task2(int[,] matrix) {
+            // code here
+
+            // end
+        }
+        public void Task3(int[,] matrix, int k) {
 
             // code here
 
             // end
 
         }
-        public void Task3(int[,] matrix, int k)
-        {
+        public void Task4(int[,] matrix) {
 
             // code here
 
             // end
 
         }
-        public void Task4(int[,] matrix)
-        {
-
-            // code here
-
-            // end
-
-        }
-        public int[,] Task5(int[,] matrix)
-        {
+        public int[,] Task5(int[,] matrix) {
             int[,] answer = null;
 
             // code here
@@ -49,16 +36,14 @@ namespace Lab5
 
             return answer;
         }
-        public void Task6(int[,] matrix)
-        {
+        public void Task6(int[,] matrix) {
 
             // code here
 
             // end
 
         }
-        public int[,] Task7(int[,] matrix, int[] array)
-        {
+        public int[,] Task7(int[,] matrix, int[] array) {
             int[,] answer = null;
 
             // code here
@@ -67,24 +52,21 @@ namespace Lab5
 
             return answer;
         }
-        public void Task8(int[,] matrix)
-        {
+        public void Task8(int[,] matrix) {
 
             // code here
 
             // end
 
         }
-        public void Task9(int[,] matrix)
-        {
+        public void Task9(int[,] matrix) {
 
             // code here
 
             // end
 
         }
-        public (int[] A, int[] B) Task10(int[,] matrix)
-        {
+        public (int[] A, int[] B) Task10(int[,] matrix) {
             int[] A = null, B = null;
 
             // code here
@@ -93,16 +75,14 @@ namespace Lab5
 
             return (A, B);
         }
-        public void Task11(int[,] matrix)
-        {
+        public void Task11(int[,] matrix) {
 
             // code here
 
             // end
 
         }
-        public void Task12(int[][] array)
-        {
+        public void Task12(int[][] array) {
 
             // code here
 
